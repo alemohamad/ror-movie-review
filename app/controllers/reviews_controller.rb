1f864e7ba582fab4480a1073893d9da08f12ecf1
Ale Mohamad <hello@alemohamad.com>
2015-01-07 14:25:21 -0300

    Convert review ratings to stars

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,9 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_movie
   before_action :authenticate_user!
 
   respond_to :html
-
-  def index
-    @reviews = Review.all
-    respond_with(@reviews)
-  end
-
-  def show
-    respond_with(@review)
-  end
 
   def new
     @review = Review.new
@@ -24,14 +16,23 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.user_id = current_user.id
-    @review.save
-    respond_with(@review)
+    @review.movie_id = @movie.id
+
+    if @review.save
+      redirect_to @movie
+    else
+      render 'new'
+    end
   end
 
   def update
     @review.user_id = current_user.id
-    @review.update(review_params)
-    respond_with(@review)
+
+    if @review.update(review_params)
+      redirect_to @movie
+    else
+      render 'edit'
+    end
   end
 
   def destroy
@@ -42,6 +43,10 @@ class ReviewsController < ApplicationController
   private
     def set_review
       @review = Review.find(params[:id])
+    end
+
+    def set_movie
+      @movie = Movie.find(params[:movie_id])
     end
 
     def review_params
